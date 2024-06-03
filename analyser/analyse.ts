118e850b0fddf15c79899d2a1cfabaa32a273469
vladaf <vladimir.filipovic@abstract.rs>
2024-06-03 23:18:35 +0200

    count languages 4

--- analyser/analyse.ts
+++ analyser/analyse.ts
@@ -5,7 +5,7 @@ import { Language, Options } from "linguist-js/dist/types";
 import { inspect } from "util";
 
 // Take directory from command line arguments
-const defaultDirectory = "../data/repos";
+const defaultDirectory = "../data/4-prt";
 const directory = process.argv[2] || defaultDirectory;
 
 // Read directory and make array of folders
@@ -16,6 +16,27 @@ const repoPaths = repoFolders.map((folder) => `${directory}/${folder}`);
 const options: Options = {
   keepVendored: false,
   quick: false,
+  ignoredLanguages: [
+    "INI",
+    "SVG",
+    "CMake",
+    "Markdown",
+    "reStructuredText",
+    "TeX",
+    "HTML",
+    "CSS",
+    "Text",
+    "GCC Machine Description",
+    "JSON",
+    "Makefile",
+    "Vim Snippet",
+    "YAML",
+    "CSV",
+    "Adblock Filter List",
+    "RMarkdown",
+    "Vim Help File",
+    "Org",
+  ],
 };
 
 const results: string[] = [];
@@ -30,14 +51,19 @@ const batchedRepoPaths = Array.from({ length: batches }, (_, i) => {
 });
 
 for (const batch of batchedRepoPaths) {
+  if (!batchedRepoPaths?.length) {
+    continue;
+  }
   const batchResults = await Promise.all(
     batch.map(async (path) => {
-      console.log(`Analyzing ${path}`);
       const result = await linguistAnalyse(path, options);
       const dominantLanguage = Object.keys(result.languages.results).sort(
         (a, b) =>
           result.languages.results[b].bytes - result.languages.results[a].bytes
       )[0];
+      if (dominantLanguage === "undefined" || dominantLanguage === undefined) {
+        return "Unknown";
+      }
       return dominantLanguage;
     })
   );
@@ -45,13 +71,12 @@ for (const batch of batchedRepoPaths) {
   results.push(...batchResults);
 }
 
-const languagesCount = results.reduce(
-  (acc: Record<Language, number>, language) => {
+const languagesCount = results
+  .filter((lang) => !!lang)
+  .reduce((acc: Record<Language, number>, language) => {
     acc[language] = acc[language] ? acc[language] + 1 : 1;
     return acc;
-  },
-  {}
-);
+  }, {});
 
 // Save json file
 writeFile("languages.json", JSON.stringify(languagesCount, null, 2));
